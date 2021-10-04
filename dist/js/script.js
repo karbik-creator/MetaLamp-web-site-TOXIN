48206cb48f00b7da986fab1e46092dfdd508d712
karbik-creator <car1na.p@yandex.ru>
2021-10-04 17:32:08 +0500

    set up functionality for select

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,55 +1,21 @@
-const getTemplate = () => {
-  return `
-    <div class="select__input" data-type="input">Сколько гостей</div>
-    <div class="select__drop__down">
-      <div class="select__drop__down__item">
-        <div class="select__item__text">Взрослые</div>
-        <div class="select__item__content__right">
-          <button class="select__item__btn minus">-</button>
-        <div class="select__item__number">0</div>
-        <button class="select__item__btn plus active">+</button>
-        </div>
-      </div>
-      <div class="select__drop__down__item">
-        <div class="select__item__text">Дети</div>
-        <div class="select__item__content__right">
-          <button class="select__item__btn minus">-</button>
-        <div class="select__item__number">0</div>
-        <button class="select__item__btn plus">+</button>
-        </div>
-      </div>
-      <div class="select__drop__down__item">
-        <div class="select__item__text">Младенцы</div>
-        <div class="select__item__content__right">
-          <button class="select__item__btn minus">-</button>
-        <div class="select__item__number">0</div>
-        <button class="select__item__btn plus">+</button>
-        </div>
-      </div>
-      <div class="select__drop__down__btn">
-        <button>очистить</button>
-        <button class="active">применить</button>
-      </div>
-    </div>
-    `;
-};
+//const getTemplate = () => {
+//};
 
 class Select {
   constructor(selector, options) {
     this.element = document.querySelector(selector);
     this.element.classList.add("select__wrapper");
-    
-    this.render();
+    this.btn = document.querySelectorAll('.select__item__btn');
+    this.numberOfPeople = [{people:12, number:12}, ]
     this.setup();
   }
 
-  render() {
-    this.clickHandler = this.clickHandler.bind(this);
-    this.element.insertAdjacentHTML("afterbegin", getTemplate());
-  }
-
   setup() {
+    this.clickHandler = this.clickHandler.bind(this);
     this.element.addEventListener("click", this.clickHandler);
+    for (let item of this.btn){
+      item.addEventListener("click", this.clickHandler);
+    }
   }
 
   clickHandler(event) {
@@ -57,6 +23,13 @@ class Select {
     if (type === 'input'){
         this.toogle();
     }
+    if (type === 'minus'){
+      
+  }
+  }
+
+  minus(){
+
   }
 
   toogle() {
