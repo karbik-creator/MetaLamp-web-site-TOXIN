a1ffe12c0e67e29764864bab12af675221ed7291
karbik <car1na.p@yandex.ru>
2021-11-14 22:43:58 +0500

    доделала раскрывающийся список

--- src/elements/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/elements/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,3 +1,24 @@
-import './expandable-checkbox-list.scss'
-import '../checkboxList/checkboxList'
+import "./expandable-checkbox-list.scss";
+import "../checkboxList/checkboxList";
+
+class Checkbox {
+  constructor(selector) {
+    this.element = document.querySelector(selector);
+    this.setup();
+  }
+
+  setup() {
+    this.toggle = this.toggle.bind(this);
+    this.expandableCheckbox = this.element.querySelectorAll(
+      "[data-type='checkbox']"
+    );
+    this.expandableCheckbox.forEach((element) => {
+      element.addEventListener("click", this.toggle);
+    });
+  }
+
+  toggle() {
+    this.element.classList.toggle("open");
+  }
+}
 
