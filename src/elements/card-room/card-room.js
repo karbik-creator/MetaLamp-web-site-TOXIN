37e7a7e6ce58322c41c46b08995ac015331fe50c
karbik-creator <car1na.p@yandex.ru>
2021-11-14 09:55:54 +0500

    редактирую функционал для dots

--- src/elements/card-room/card-room.js
+++ src/elements/card-room/card-room.js
@@ -3,36 +3,54 @@ import "./img/slider1.jpg";
 import "./img/slider2.jpg";
 import "./img/slider3.jpg";
 import "../rateTrack/rateTrack";
-
 const slider = document.querySelector(".slider");
-const sliderItems = document.querySelectorAll(".slider__item");
+const sliderWrapper = document.querySelector(".slider__wrapper");
+const sliderItems = slider.querySelectorAll(".slider__item");
+const dotContainer = document.querySelector(".dots");
 let isDown = false;
 let startX;
 let scrollLeft;
 let walk;
+let dots;
+let widthSlider = parseInt(slider.style.width);
 
-slider.addEventListener("mousedown", (e) => {
-isDown = true;
-startX = e.pageX - slider.offsetLeft;
-scrollLeft = slider.scrollLeft;
+function renderDots() {
+    dotContainer.innerHTML = '';
+    sliderItems.forEach((item, index) => {
+        let dot = document.createElement("div");
+        index === 0 ? dot.classList.add('dot', 'active') : dot.classList.add('dot');
+        dotContainer.append(dot);
+    });
+    dots = dotContainer.querySelectorAll('.dot');
+};
+renderDots();
+
+
+sliderWrapper.addEventListener("mousedown", (e) => {
+    isDown = true;
+    startX = e.pageX - sliderWrapper.offsetLeft;
+    scrollLeft = sliderWrapper.scrollLeft;
 });
 
-slider.addEventListener("mouseleave", () => {
-isDown = false;
+sliderWrapper.addEventListener("mouseup", (e) => {
+    isDown = false;
+    let numberSlider = parseInt(e.target.dataset.item) - 1;
+    /*dots.forEach((item, index) => {
+        index === numberSlider ?
+            item.classList.add('active') : item.classList.remove('active')
+    });*/
+    sliderWrapper.scrollLeft = (parseInt(e.target.dataset.item) - 1) * 270;
 });
 
-slider.addEventListener("mouseup", (e) => {
-isDown = false;
-if (walk < 0) {
-slider.scrollLeft = (parseInt(e.target.dataset.item) - 1) * 270;
-} else {
-slider.scrollLeft = (parseInt(e.target.dataset.item) - 1) * 270;
-}
+sliderWrapper.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    walk = (x - startX);
+    console.log(x - startX);
+    sliderWrapper.scrollLeft = scrollLeft - walk;
 });
-slider.addEventListener("mousemove", (e) => {
-if (!isDown) return;
-e.preventDefault();
-const x = e.pageX - slider.offsetLeft;
-walk = (x - startX) * 3;
-slider.scrollLeft = scrollLeft - walk;
+
+sliderWrapper.addEventListener("mouseleave", (e) => {
+    isDown = false;
 });
